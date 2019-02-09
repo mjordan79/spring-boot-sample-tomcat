03682f61a1c38549088ece10863a4006680928f5
Renato Perini <renato.perini@gmail.com>
2019-02-09 21:14:25 +0100

    Reorganized services and information returned.

--- src/main/java/sample/tomcat/web/SampleController.java
+++ src/main/java/sample/tomcat/web/SampleController.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package sample.tomcat.web;
 
 import sample.tomcat.service.HelloWorldService;
@@ -28,8 +12,14 @@ public class SampleController {
 
 	@Autowired
 	private HelloWorldService helloWorldService;
-
+	
 	@GetMapping("/")
+	@ResponseBody
+	public String getIPAddresss() {
+	    return "Application running on pod: " + this.helloWorldService.getHostname();
+	}
+	
+	@GetMapping("/hello")
 	@ResponseBody
 	public String helloWorld() {
 		return this.helloWorldService.getHelloMessage();
